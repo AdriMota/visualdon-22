e4af095c8c8d530379eb76d0e95392d6f532bb86
Adriana Mota <adrianamota17@hotmail.com>
2022-03-03 14:22:40 +0100

    Rendu exercice 1

--- 01-SVG/js/script.js
+++ 01-SVG/js/script.js
@@ -2,8 +2,17 @@
 * Changes the color of the SVG rectangle with ID 'myrect'
 */
 
-let rect = document.querySelectorAll('.rect');
+let rect = document.querySelector('.rect');
+let donut = document.querySelector('.donut');
 
 rect.addEventListener("click", evt => {
-    rect.classList.toggle('.lightblue');
+    rect.classList.toggle('color');
+})
+
+donut.addEventListener("mouseover", evt => {
+    donut.setAttribute('r', '75');
+})
+
+donut.addEventListener("mouseout", evt => {
+    donut.setAttribute('r', '60');
 })
