c827b00db4ce5f4a56454c135775250794c73cb3
Adriana Mota <adrianamota17@hotmail.com>
2022-03-31 21:37:47 +0200

    Consigne exercice 4

--- 04-gapminder/src/index.js
+++ 04-gapminder/src/index.js
@@ -1,4 +1,8 @@
-import * as d3 from 'd3'
+import * as d3 from 'd3';
+
+import file from '../data/income_per_person_gdppercapita_ppp_inflation_adjusted.csv';
+//import file from '../data/life_expectancy_years.csv';
+//import file from '../data/population_total.csv';
 
 // Pour importer les données
 // import file from '../data/data.csv'
